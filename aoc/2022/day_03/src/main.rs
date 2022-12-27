ee9c4f30ac92e5c788fda0b682665d35cc089a3d
lbellomo <lbellomo@gmail.com>
2022-12-27 18:30:18 -0300

    clean code

--- aoc/2022/day_03/src/main.rs
+++ aoc/2022/day_03/src/main.rs
@@ -1,3 +1,10 @@
+fn get_priority_value(priority: char) -> u32 {
+    match priority.is_lowercase() {
+        true => priority as u32 + 1 - 'a' as u32,
+        false => priority as u32 + 1 - 'A' as u32 + 26,
+    }
+}
+
 fn main() {
     let input = include_str!("input.txt");
 
@@ -6,29 +13,17 @@ fn main() {
         .map(|line| {
             let (first, second) = line.split_at(line.len() / 2);
             let priority = first.chars().find(|&x| second.contains(x)).unwrap();
-            dbg!(priority, priority as u32);
-
-            match priority.is_lowercase() {
-                true => priority as u32 + 1 - 'a' as u32,
-                false => priority as u32 + 1 - 'A' as u32 + 26,
-            }
+            get_priority_value(priority)
         })
         .sum();
 
     let vec_input = input.lines().collect::<Vec<_>>();
-
-    // for group in vec_input.chunks(3) {
     let sol_b: u32 = vec_input
         .chunks(3)
         .map(|group| {
-            // let hashset = HashSet::new();
             if let [a, b, c] = group {
                 let priority = a.chars().find(|&x| b.contains(x) && c.contains(x)).unwrap();
-
-                match priority.is_lowercase() {
-                    true => priority as u32 + 1 - 'a' as u32,
-                    false => priority as u32 + 1 - 'A' as u32 + 26,
-                }
+                get_priority_value(priority)
             } else {
                 panic!("Invalid input")
             }
